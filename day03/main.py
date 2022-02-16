8dacf1afd512cc4ced1d837d90e355eb8154e5b4
phillipai <98851253+phillipai@users.noreply.github.com>
2022-02-16 10:58:23 -0500

    Create main.py

---
+++ day03/main.py
@@ -0,0 +1,7 @@
+# Day 3 - Control Flow and Logical Operators
+
+## Tip Calculator
+
+![day03](https://user-images.githubusercontent.com/98851253/154304693-a3aa3a5a-e8f3-46b8-bcea-6f0884aad99c.gif)
+
+https://replit.com/@phillipra/treasure-island
