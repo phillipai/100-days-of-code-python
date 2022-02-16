9b95cc959024491a845129b7265d955768853da1
Phillip <98851253+phillipai@users.noreply.github.com>
2022-02-16 11:35:36 -0500

    Create main.py

---
+++ day06/main.py
@@ -0,0 +1,17 @@
+def turn_right():    
+    turn_left()
+    turn_left()
+    turn_left()
+    
+while front_is_clear():
+    move()
+turn_left()
+    
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
