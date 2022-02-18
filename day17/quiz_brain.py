1ff2234d00e74212011d6a667b8181b9249f8ed5
Phillip <98851253+phillipai@users.noreply.github.com>
2022-02-18 11:00:22 -0500

    Create quiz_brain.py

---
+++ day17/quiz_brain.py
@@ -0,0 +1,26 @@
+class QuizBrain:
+
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+        self.score = 0
+
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's incorrect")
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is {self.score}/{self.question_number}.")
+        print("\n")
+        
