7c8c71cc2e9b632777f9657b548f4d33dcfd9260
Phillip <98851253+phillipai@users.noreply.github.com>
2022-02-19 21:11:42 -0500

    Create player.py

---
+++ day23/player.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.setheading(90)
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.goto(STARTING_POSITION)
+
+    def move_up(self):
+        new_y = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_y)
+
+    def starting_position(self):
+        self.goto(STARTING_POSITION)
