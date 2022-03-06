83a3bc3da1c2acfc87339143703526de9e6d11ad
Phillip <98851253+phillipai@users.noreply.github.com>
2022-03-06 17:13:49 -0500

    Create main.py

---
+++ day39/main.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+from notification_manager import NotificationManager
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
+
+ORIGIN_CITY_IATA = "LON"
+
+if sheet_data[0]["iataCode"] == "":
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
+    if flight.price < destination["lowestPrice"]:
+        notification_manager.telegram_bot_send_text(
+            f"Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} "
+            f"to {flight.destination_city}-{flight.destination_airport}, "
+            f"from {flight.out_date} to {flight.return_date}."
+        )
