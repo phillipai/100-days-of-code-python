bd8c0d8d81fb5e7b440da57705abb95c41e8c20d
Phillip <98851253+phillipai@users.noreply.github.com>
2022-03-07 12:22:49 -0500

    Create data_manager.py

---
+++ day40/data_manager.py
@@ -0,0 +1,36 @@
+import requests
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/********************************/flightDeals/prices/"
+SHEETY_USERS_ENDPOINT = "https://api.sheety.co/*********************************/flightDeals/users"
+
+
+class DataManager:
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response.text)
+
+    def get_customer_emails(self):
+        customers_endpoint = SHEETY_USERS_ENDPOINT
+        response = requests.get(url=customers_endpoint)
+        data = response.json()
+        self.customer_data = data["users"]
+        return self.customer_data
