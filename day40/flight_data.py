d42c6506da8b012356ebbf24a6103a5814385856
Phillip <98851253+phillipai@users.noreply.github.com>
2022-03-07 12:23:04 -0500

    Create flight_data.py

---
+++ day40/flight_data.py
@@ -0,0 +1,13 @@
+class FlightData:
+
+    def __init__(
+        self, price, origin_city, origin_airport, destination_city, destination_airport, out_date, return_date, stop_overs=0, via_city=""):
+        self.price = price
+        self.origin_city = origin_city
+        self.origin_airport = origin_airport
+        self.destination_city = destination_city
+        self.destination_airport = destination_airport
+        self.out_date = out_date
+        self.return_date = return_date
+        self.stop_overs = stop_overs
+        self.via_city = via_city
