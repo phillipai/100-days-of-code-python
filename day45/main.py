4259190e936bf7ce95fb7b851c9f129cb4cbe305
Phillip <98851253+phillipai@users.noreply.github.com>
2022-03-12 22:55:08 -0500

    Create main.py

---
+++ day45/main.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
+
+all_movies = soup.find_all(name="h3", class_="title")
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        print(movie)
+        file.write(f"{movie}\n")
