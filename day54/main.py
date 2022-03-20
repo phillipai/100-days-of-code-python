adef6e8e02d4c08954fa45ba6751e38af751e601
Phillip <98851253+phillipai@users.noreply.github.com>
2022-03-20 11:45:33 -0400

    Create main.py

---
+++ day54/main.py
@@ -0,0 +1,28 @@
+import time
+current_time = time.time()
+print(current_time)
+
+
+def speed_calc_decorator(function):
+    def wrapper_function():
+        start_time = time.time()
+        function()
+        end_time = time.time()
+        print(f"{function.__name__} run time = {end_time - start_time} second(s)")
+    return wrapper_function
+
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(10000000):
+        i * i
+
+
+@speed_calc_decorator
+def slow_function():
+    for i in range(100000000):
+        i * i
+
+
+fast_function()
+slow_function()
