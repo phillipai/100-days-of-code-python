1c0aacd75b9c6350525c767572ce98b74d47a857
Phillip <98851253+phillipai@users.noreply.github.com>
2022-03-22 19:41:12 -0400

    Add files via upload

---
+++ day56/server.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
