a279ae3f7da4b6f8c43b593c3a51c2fdf21879c2
Phillip <98851253+phillipai@users.noreply.github.com>
2022-04-03 21:54:48 -0400

    Update main.py

--- day62/main.py
+++ day62/main.py
@@ -20,14 +20,6 @@ class CafeForm(FlaskForm):
     power_rating = SelectField("Power Socket Availability", choices=["✘", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"], validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-# Exercise:
-# add: Location URL, open time, closing time, coffee rating, wifi rating, power outlet rating fields
-# make coffee/wifi/power a select element with choice of 0 to 5.
-#e.g. You could use emojis ☕️/💪/✘/🔌
-# make all fields required except submit
-# use a validator to check that the URL field has a URL entered.
-# ---------------------------------------------------------------------------
-
 
 # all Flask routes below
 @app.route("/")
