6e9e0624dc420592756fdeaf0fd07033dd76957b
jinman36 <jeffery.inman@hotmail.com>
2022-08-19 21:27:28 -0700

    added automateTheBoringStuff to day 1 of code

---
+++ day01/automateTheBoringStuff.py
@@ -0,0 +1,35 @@
+#Using the 100 days of code and Humble bundle books to get back intot he swing of pyhton
+
+#Day 1
+#print('hello world')
+
+#Common Math Operators
+# Operator  Operation                               Example       Evaluates to . . .
+# **        Exponent                                2 ** 3        8
+# %         Modulus/remainder                       22 % 8        6
+# //        Integer division/floored quotient       22 // 8       2
+# /         Division                                22 / 8        2.75
+# *         Multiplication                          3 * 5         15
+# -         Subtraction                             5 - 2         3
+# +         Addition                                2 + 2         4
+
+# First Program - This program says hello and asks for my name
+
+print('Hello, world!')
+print('what is your name?')
+myName = input()
+print('its great to meet you, ', myName)
+nameLength = len(myName)
+print('the length of your name is: ', nameLength)
+# print(len(myName))
+print('what is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+
+
+
+
+
+
+
+
