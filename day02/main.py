169f92c1de58b6bb422ba8c18c58a8e8c57a6c5f
Vishnu Mohandas <53164179+vishnuak15@users.noreply.github.com>
2023-01-09 10:57:44 +0530

    added comments for data types

--- day02/main.py
+++ day02/main.py
@@ -1,9 +1,9 @@
 print("Welcome to the tip calculator!")
 
-bill = float(input("What is the total bill amount?\n$:"))
-tip = int(input("How much tip would you like to give?\nPercent:"))
+bill = float(input("What is the total bill amount?\n$:")) # takes input of float data type 
+tip = int(input("How much tip would you like to give?\nPercent:")) # takes input of int data type 
 split = int(input("How many people to split the bill?\nPeople:"))
 
-total = ("{:.2f}".format((((bill * (tip / 100)) + bill) / split)))
+total = ("{:.2f}".format((((bill * (tip / 100)) + bill) / split))) 
 
 print(f"Each person should pay: ${total}")
