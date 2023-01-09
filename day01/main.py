53446e1114beb3638b33dc321fd0fc0fa23b8479
Vishnu Mohandas <53164179+vishnuak15@users.noreply.github.com>
2023-01-09 10:53:27 +0530

    added comments

--- day01/main.py
+++ day01/main.py
@@ -1,4 +1,4 @@
-print("Welcome to The Band Name Generator!")
-city = input("What city did you grow up in?\n")
-pet = input("What is the name of your first pet?\n")
-print("Your band name is " + city +  " " + pet)
+print("Welcome to The Band Name Generator!") #prints what is inside the double qoutes
+city = input("What city did you grow up in?\n") #getting the input from the user
+pet = input("What is the name of your first pet?\n") #getting the input from the user
+print("Your band name is " + city +  " " + pet) #prints the variable name that is being passed
